7764f2c36e7c0a023adde899fd0c9b7ae3ab6ef3
avijitg_intd <avijit.ghosh@indusnet.co.in>
2025-11-07 16:57:43 +0530

    test node server runnning

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Node REST API!" });
+});
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+
+app.post("/echo", (req, res) => {
+  res.json({ youSent: req.body });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`URL: http://localhost:${PORT}`);
+});
